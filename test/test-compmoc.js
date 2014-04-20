8b67892c296b11177e95b08256bcf36804a5eccc
Andrew Rea <andrew.rea@hp.com>
2014-04-20 10:08:14 +0100

    grunted, linted and covered

--- test/test-compmoc.js
+++ test/test-compmoc.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var compmoc = require('../lib/compmoc.js');
+var util = require('util');
 
 
 describe('Wrapping existing object with expectations', function() {
@@ -8,10 +9,10 @@ describe('Wrapping existing object with expectations', function() {
     var Person = function(name) {
         return Object.freeze({
             greet: function(otherPersonName) {
-                console.log(name, 'says hello to', otherPersonName);
+               return util.format('%s says hello to %s', name, otherPersonName);
             }
         });
-    }
+    };
 
     it('enables counting the number of invocations of a method', function(done) {
         var bob = new Person('bob');
