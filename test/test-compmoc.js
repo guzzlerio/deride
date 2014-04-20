6be6f096f490cb6ebba24f5cc81748c3c3dce183
Andrew Rea <andrew.rea@hp.com>
2014-04-20 10:35:25 +0100

    Basic support for stubbing

--- test/test-compmoc.js
+++ test/test-compmoc.js
@@ -3,19 +3,29 @@
 var compmoc = require('../lib/compmoc.js');
 var util = require('util');
 
+describe('Creating a stub object', function() {
+
+    var bob = compmoc.stub(['greet']);
+
+    assertions(bob);
+});
+
 
 describe('Wrapping existing object with expectations', function() {
 
     var Person = function(name) {
         return Object.freeze({
             greet: function(otherPersonName) {
-               return util.format('%s says hello to %s', name, otherPersonName);
+                return util.format('%s says hello to %s', name, otherPersonName);
             }
         });
     };
 
+    assertions(new Person('non'));
+});
+
+function assertions(bob){
     it('enables counting the number of invocations of a method', function(done) {
-        var bob = new Person('bob');
         bob = compmoc.wrap(bob);
         bob.greet('alice');
         bob.expect.greet.called.times(1);
@@ -23,19 +33,16 @@ describe('Wrapping existing object with expectations', function() {
     });
 
     it('enables the determination that a method has NEVER been called', function(done) {
-        var bob = new Person('bob');
         bob = compmoc.wrap(bob);
         bob.expect.greet.called.never();
         done();
     });
 
     it('enables the determination of the args used to invoke the method', function(done) {
-        var bob = new Person('bob');
         bob = compmoc.wrap(bob);
         bob.greet('alice');
         bob.greet('bob');
         bob.expect.greet.called.withArgs('bob');
         done();
     });
-
-});
+}
