a2ebf900cd2514535c50709feb3ff260ab759fe1
Andrew Rea <andrew.rea@hp.com>
2014-04-21 09:59:33 +0100

    enables overriding a methods body when specific arguments are provided

--- test/test-compmoc.js
+++ test/test-compmoc.js
@@ -98,6 +98,21 @@ _.forEach(tests, function(test) {
             done();
         });
 
+        it('enables overriding a methods body when specific arguments are provided', function(done) {
+            bob = compmoc.wrap(bob);
+            bob.setup.greet.when('alice').toDoThis(function(otherPersonName) {
+                return util.format('yo yo %s', otherPersonName);
+            });
+            bob.setup.greet.toDoThis(function(otherPersonName) {
+                return util.format('yo %s', otherPersonName);
+            });
+            var result1 = bob.greet('alice');
+            var result2 = bob.greet('bob');
+            result1.should.eql('yo yo alice');
+            result2.should.eql('yo bob');
+            done();
+        });
+
         it('enables setting the return value of a function when specific arguments are provided', function(done) {
             bob = compmoc.wrap(bob);
             bob.setup.greet.when('alice').toReturn('foobar');
